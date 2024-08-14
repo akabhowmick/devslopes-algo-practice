220504d028e518157ce3b623905d7c429b4f289f
Akash <akabhowmick@gmail.com>
2024-08-14 14:36:38 -0400

    solve fizzbuzz

--- Week1/FizzBuzz.py
+++ Week1/FizzBuzz.py
@@ -1,3 +1,16 @@
-#Write a function that prints numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number, and for multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
+# Write a function that prints numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number, and for multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
 
 
+def fizzBuzz():
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:  # Check if number is divisible by both 3 and 5
+            print("FizzBuzz")
+        elif i % 3 == 0:  # Check if number is divisible by 3
+            print("Fizz")
+        elif i % 5 == 0:  # Check if number is divisible by 5
+            print("Buzz")
+        else:  # Print the number itself
+            print(i)
+
+
+fizzBuzz()
