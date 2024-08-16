04bcd7c6bb3d1b975c3d5c63e5ffd0760349f99d
Akash <akabhowmick@gmail.com>
2024-08-16 09:37:04 -0400

    solve IsPalindrome

--- Week2/IsPalindrome.py
+++ Week2/IsPalindrome.py
@@ -1,1 +1,19 @@
-# Implement a function that takes a string and returns an object with the counts of vowels and consonants.
+# Prompt: Write a function that checks if a given string is a palindrome (reads the same forwards and backwards). Ensure the function ignores spaces, punctuation, and case differences when checking for a palindrome.
+# Extension: Write a recursive version of this function.
+
+
+def isPalindrome(str):
+    str = str.lower()
+    dummy = []
+    for i in str:
+        if i.isalpha():
+            dummy.append(i)
+    for i in range(0, len(dummy)):
+        if dummy[i] != dummy[len(dummy) - i - 1]:
+            return False
+    return True
+
+
+print(isPalindrome("anna21323fasqas"))
+print(isPalindrome("RaceCar"))
+print(isPalindrome("Race,123,Car"))
