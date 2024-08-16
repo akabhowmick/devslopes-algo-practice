2beee8f613d0c09ab8cb45cdd14f93052a914714
Akash <akabhowmick@gmail.com>
2024-08-15 22:04:03 -0400

    solve CreateDictionary

--- Week2/CreateDictionary.py
+++ Week2/CreateDictionary.py
@@ -1,1 +1,19 @@
 # Implement a function that takes a string and returns an object with the counts of vowels and consonants.
+
+
+def create_count(str):
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    vowel_count = 0
+    consonant_count = 0
+
+    for char in str:
+        if char in vowels:
+            vowel_count += 1
+        elif char.isalpha():
+            consonant_count += 1
+
+    return {"vowels": vowel_count, "consonants": consonant_count}
+
+
+print(create_count("Hello World"))
+print(create_count("Python"))
