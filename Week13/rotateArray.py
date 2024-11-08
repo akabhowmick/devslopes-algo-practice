f00672dca5c209b9efb96cc821e9a91427b63003
Akash <akabhowmick@gmail.com>
2024-11-08 09:15:30 -0500

    week 13 update

--- Week13/rotateArray.py
+++ Week13/rotateArray.py
@@ -3,16 +3,17 @@
 
 def rotate_array(nums, k):
     k = k % len(nums)
-    nums[k:] = nums[len(nums) - k:] + nums[:len(nums) - k]  # Rotate the array to the right
-    return nums
+    nums[:] = nums[len(nums) - k :] + nums[: len(nums) - k]
 
 
 # Extension: Modify the function to handle negative values of k to rotate the array to the left.
 def rotate_array_with_neg(nums, k):
     k = len(nums) + k % len(nums)  # Calculate the absolute value of k
-    nums[k:] = nums[:len(nums) - k] + nums[len(nums) - k:]  # Rotate the array to the left
+    nums[k:] = (
+        nums[: len(nums) - k] + nums[len(nums) - k :]
+    )  # Rotate the array to the left
     return nums
-  
+
 
 # Example usage:
 nums = [1, 2, 3, 4, 5]
@@ -22,4 +23,3 @@ print(rotate_array(nums, k))  # Output: [3, 4, 5, 1, 2]
 nums = [1, 2, 3, 4, 5]
 k = -2
 print(rotate_array_with_neg(nums, k))  # Output: [5, 1, 2, 3, 4]
-  
